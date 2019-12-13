a9f5f0764b16943d84c5f52f516a2bdeae854eb4
Ross Whitehouse <ross.dw@live.com>
2019-12-13 19:30:36 +0000

    Initialize Christmas.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,48 @@
 
-$(function() {
+$(function () {
+
+    WTF.init({
+        heading: [
+            "You should watch..."
+        ],
+        response: [
+            "We watched this one. Next!",
+            "Gimme another.",
+            "Let's go again.",
+            "More Christmas please"
+        ],
+        template: [
+            "<p id='title'>@movie</p> <p id='signoff'>@signoff</p>"
+        ],
+        movie: [
+            "The Santa Clause",
+            "The Polar Express",
+            "Klaus",
+            "Muppets Christmas Carol",
+            "Home Alone",
+            "The Grinch",
+            "Elf",
+            "Christmas with the Coopers",
+            "Love Actually",
+            "A Charlie Brown Christmas",
+            "Scrooged",
+            "The Christmas Prince",
+            "The Christmas Switch"
+        ],
+        signoff: [
+            "Merry fucking Christmas",
+            "and a partridge in a pear tree",
+            "Ya filthy animal",
+            "You better watch out. You better not cry.",
+            "You cotton-headed ninny-muggins.",
+            "Santa's coming.",
+            "No, Die Hard is not on the list",
+            "Hope you're using recyclable paper. Or else.",
+            "Maybe Christmas doesn't come from a store. Maybe Christmas, perhaps, means a little bit more.",
+            "Help me... I'm feeling.",
+            "A lot of these movies seem a little problematic now, don't they?"
+        ]
+    })
 
     // Populate using an object literal
 
@@ -36,6 +79,6 @@ $(function() {
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
+    // WTF.init('0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc');
 
 });
